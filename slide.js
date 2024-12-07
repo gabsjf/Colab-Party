51b510682a8d84e47966c44db296829a4c2e205c
Gabriel <115955863+gabsjf@users.noreply.github.com>
2024-12-07 10:16:40 -0400

    Programação do dia.js

--- slide.js
+++ slide.js
@@ -1,11 +1,29 @@
 let currentIndex = 0;
 
-function nextSlide() {
+
+const backgroundColors = ['#240506', '#06090f', '#06090f', '#06090f', '#000000', '#000000', '#06090f', '#000000']; 
+
+
+function updateSlide(index) {
     const slides = document.querySelectorAll('.slider-content .slide');
     const slider = document.querySelector('.slider-content');
-    currentIndex = (currentIndex + 1) % slides.length;
+    const body = document.body;
+
+
+    currentIndex = (index + slides.length) % slides.length;
+
+   
     slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+  
+    body.style.backgroundColor = backgroundColors[currentIndex];
 }
 
-// Intervalo automático para mudar os slides
-setInterval(nextSlide, 5000); // Muda a cada 3 segundos
+
+document.getElementById('prev').addEventListener('click', () => {
+    updateSlide(currentIndex - 1);
+});
+
+document.getElementById('next').addEventListener('click', () => {
+    updateSlide(currentIndex + 1); 
+});
